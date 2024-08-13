cfcc12400948a33e7e8902c12378479a1333399e
akshatabakre <148846716+akshatabakre@users.noreply.github.com>
2024-08-13 14:01:29 +0530

    Time: 95 ms (13%), Space: 52 MB (5.13%) - LeetHub

--- 2402-maximum-xor-after-operations/2402-maximum-xor-after-operations.cpp
+++ 2402-maximum-xor-after-operations/2402-maximum-xor-after-operations.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     int maximumXOR(vector<int>& nums) {
         int x = 0;
+        sort(nums.begin(),nums.end());
         for(int i=0;i<nums.size();i++){
             x = x|nums[i];
         }
