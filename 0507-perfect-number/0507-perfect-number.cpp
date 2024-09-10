de5acdefcb7d333a5e54e1742fd7fd56127c876b
Akshata Bakre <148846716+akshatabakre@users.noreply.github.com>
2024-09-10 19:15:17 +0530

    Time: 0 ms (100%), Space: 7.3 MB (59.87%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.cpp
+++ 0507-perfect-number/0507-perfect-number.cpp
@@ -1,11 +1,15 @@
 class Solution {
 public:
     bool checkPerfectNumber(int num) {
-        int sumofdiv = 0;
-        for(int i=1;i<num;i++){
+        int sumofdiv = 1;
+        if(num==1){
+            return false;
+        }
+        for(int i=2;i<sqrt(num);i++){
             if(num%i==0){
                 cout<<i<<" is divisor"<<endl;
                 sumofdiv+=i;
+                sumofdiv+=(num/i);
                 cout<<sumofdiv<<endl;
                 if(sumofdiv>num){
                     return false;
