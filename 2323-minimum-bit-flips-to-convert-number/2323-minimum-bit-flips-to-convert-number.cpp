d9f160c595d587d9861dc3ef543ef179940f3d28
Akshata Bakre <148846716+akshatabakre@users.noreply.github.com>
2024-09-11 09:42:30 +0530

    Time: 4 ms (41.47%), Space: 7.7 MB (44.87%) - LeetHub

--- 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.cpp
+++ 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.cpp
@@ -1,10 +1,15 @@
 class Solution {
 public:
     int minBitFlips(int start, int goal) {
-        int xored = start^goal;
+        // int xored = start^goal;
         int ans = 0;
         for(int i=0;i<32;i++){
-            if((xored>>i)&1){
+            // if((xored>>i)&1){
+            //     ans++;
+            // }
+            int n1 = (start>>i)&1;
+            int n2 = (goal>>i)&1;
+            if(n1!=n2){
                 ans++;
             }
         }
