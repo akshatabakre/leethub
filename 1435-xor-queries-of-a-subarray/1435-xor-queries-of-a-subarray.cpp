149374d559afba4f87c8d6d5043e15a7be8f9b58
Akshata Bakre <148846716+akshatabakre@users.noreply.github.com>
2024-09-13 17:31:17 +0530

    Time: 1414 ms (12.57%), Space: 41.1 MB (99.32%) - LeetHub

--- 1435-xor-queries-of-a-subarray/1435-xor-queries-of-a-subarray.cpp
+++ 1435-xor-queries-of-a-subarray/1435-xor-queries-of-a-subarray.cpp
@@ -3,15 +3,11 @@ public:
     vector<int> xorQueries(vector<int>& arr, vector<vector<int>>& queries) {
         vector<int> answer(queries.size());
         for(int i=0;i<queries.size();i++){
-            if(queries[0]==queries[1]){
-                answer[i] = 0;
-            }else{
-                int a = 0;
-                for(int j=queries[i][0];j<=queries[i][1];j++){
-                    a = a^arr[j];
-                }
-                answer[i] = a;
+            int a = 0;
+            for(int j=queries[i][0];j<=queries[i][1];j++){
+                a = a^arr[j];
             }
+            answer[i] = a;
         }
         return answer;
     }
