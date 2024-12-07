1020fb238c7730e6253b76b23d9b9e39bc917ed4
Akshata Bakre <148846716+akshatabakre@users.noreply.github.com>
2024-12-07 21:30:59 +0530

    Time: 0 ms (100%), Space: 10.6 MB (26.9%) - LeetHub

---
+++ 0046-permutations/0046-permutations.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void perm(vector<vector<int>>& ans, vector<int>& v,vector<int>& nums,vector<bool>& picked){
+        if(v.size()==nums.size()){
+            ans.push_back(v);
+            return;
+        }
+        for(int i=0;i<nums.size();i++){
+            if(picked[i]==false){
+                picked[i] = true;
+                v.push_back(nums[i]);
+                perm(ans,v,nums,picked);
+                v.pop_back();
+                picked[i] = false;
+            }
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> v;
+        vector<bool> picked(nums.size(),false);
+        perm(ans,v,nums,picked);
+        return ans;
+    }
+};
