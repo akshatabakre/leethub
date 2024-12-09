5f5d4863eda2eb352aeb218575a0abed1086e29b
Akshata Bakre <148846716+akshatabakre@users.noreply.github.com>
2024-12-09 12:43:29 +0530

    Time: 431 ms (17.23%), Space: 8.8 MB (26.74%) - LeetHub

---
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool solve(vector<vector<char>>& board){
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+
+                if(board[i][j]=='.'){//empty space encountered
+                    for(char c='1';c<='9';c++){
+                        if(isValid(board,i,j,c)){
+                            board[i][j] = c;//trying c
+
+                            if(solve(board)){
+                                return true;//found a solution
+                            }else{
+                                board[i][j] = '.';//backtracking
+                            }
+                        }
+                    }//couldn't find any solution for the blank space, hence
+                    return false;
+                }
+            }
+        }
+        //out of the traversing loop, ie no blank space left to be decided, all filled
+        return true;
+    }
+    bool isValid(vector<vector<char>>& board,int row,int col,char c){
+        for(int i=0;i<9;i++){
+            if(board[i][col]==c)
+                return false;
+            if(board[row][i]==c)
+                return false;
+            if(board[3*(row/3)+i/3][3*(col/3)+i%3]==c)
+                return false;
+        }
+        return true;
+    }
+    void solveSudoku(vector<vector<char>>& board) {
+        solve(board);
+    }
+};
