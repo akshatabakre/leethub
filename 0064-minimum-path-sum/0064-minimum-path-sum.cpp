6218d1ea9c87d9358bf90a1fa6eff97090941178
Akshata Bakre <148846716+akshatabakre@users.noreply.github.com>
2024-12-16 18:41:01 +0530

    Time: 3 ms (30.5%), Space: 16 MB (22.46%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -17,25 +17,45 @@ public:
     // }
     int minPathSum(vector<vector<int>>& grid) {
         int m = grid.size(), n = grid[0].size();
-        vector<vector<int>> dp(m,vector<int> (n,-1));
+        // vector<vector<int>> dp(m,vector<int> (n,-1));
         // dp[0][0] = grid[0][0];
         // return f(m-1,n-1,grid,dp);
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(i==0 && j==0){
-                    dp[i][j] = grid[0][0];
-                    continue;
-                }
-                int up = INT_MAX, left = INT_MAX;
-                if(i>0){
-                    up = dp[i-1][j];
-                }
-                if(j>0){
-                    left = dp[i][j-1];
-                }
-                dp[i][j] = grid[i][j] + min(up,left);
-            }
+        // for(int i=0;i<m;i++){
+        //     for(int j=0;j<n;j++){
+        //         if(i==0 && j==0){
+        //             dp[i][j] = grid[0][0];
+        //             continue;
+        //         }
+        //         int up = INT_MAX, left = INT_MAX;
+        //         if(i>0){
+        //             up = dp[i-1][j];
+        //         }
+        //         if(j>0){
+        //             left = dp[i][j-1];
+        //         }
+        //         dp[i][j] = grid[i][j] + min(up,left);
+        //     }
+        // }
+        // return dp[m-1][n-1];
+
+        //space optimization
+        vector<int> v(n);
+        //base case initialization
+        v[0] = grid[0][0];
+        for(int i=1;i<n;i++){
+            v[i] = v[i-1] + grid[0][i];
         }
-        return dp[m-1][n-1];
+        for(int i=1;i<m;i++){
+            vector<int> temp(n);
+            for(int j=0;j<n;j++){
+                int up = v[j],left = INT_MAX;
+                if(j>0){
+                    left = temp[j-1];
+                }
+                temp[j] = grid[i][j] + min(up,left);
+            }
+            v = temp;
+        }
+        return v[n-1];
     }
 };
