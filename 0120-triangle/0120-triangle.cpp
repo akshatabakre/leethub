226bd33a1fbed2660db45e02a5342aeacb4b193d
Akshata Bakre <148846716+akshatabakre@users.noreply.github.com>
2024-12-16 21:11:17 +0530

    Time: 0 ms (100%), Space: 13.3 MB (9.37%) - LeetHub

---
+++ 0120-triangle/0120-triangle.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int f(int i,int j,vector<vector<int>>& t,int n,vector<vector<int>>& dp){
+        if(i==n-1){
+            return t[n-1][j];
+        }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        int d = f(i+1,j,t,n,dp);
+        int dg = f(i+1,j+1,t,n,dp);
+        return dp[i][j] = t[i][j] + min(d,dg);
+    }
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int n = triangle.size();
+        vector<vector<int>> dp(n,vector<int> (n,-1));
+        return f(0,0,triangle,n,dp);
+    }
+};
