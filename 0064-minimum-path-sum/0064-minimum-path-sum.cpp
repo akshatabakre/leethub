b03a2ccd12bfcd9c60f63613413c4e73f3d2f657
Akshata Bakre <148846716+akshatabakre@users.noreply.github.com>
2024-12-16 18:41:31 +0530

    Time: 0 ms (100%), Space: 16.1 MB (20.97%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,43 +1,7 @@
 class Solution {
 public:
-//using recursion
-    // int f(int i,int j,vector<vector<int>>& grid,vector<vector<int>>&dp){
-    //     if(i==0 && j==0){
-    //         return grid[0][0];
-    //     }
-    //     if(i<0 || j<0){
-    //         return INT_MAX;
-    //     }
-    //     if(dp[i][j]!=-1){
-    //         return dp[i][j];
-    //     }
-    //     int up = f(i-1,j,grid,dp);
-    //     int left = f(i,j-1,grid,dp);
-    //     return dp[i][j] = grid[i][j] + min(up,left);
-    // }
     int minPathSum(vector<vector<int>>& grid) {
         int m = grid.size(), n = grid[0].size();
-        // vector<vector<int>> dp(m,vector<int> (n,-1));
-        // dp[0][0] = grid[0][0];
-        // return f(m-1,n-1,grid,dp);
-        // for(int i=0;i<m;i++){
-        //     for(int j=0;j<n;j++){
-        //         if(i==0 && j==0){
-        //             dp[i][j] = grid[0][0];
-        //             continue;
-        //         }
-        //         int up = INT_MAX, left = INT_MAX;
-        //         if(i>0){
-        //             up = dp[i-1][j];
-        //         }
-        //         if(j>0){
-        //             left = dp[i][j-1];
-        //         }
-        //         dp[i][j] = grid[i][j] + min(up,left);
-        //     }
-        // }
-        // return dp[m-1][n-1];
-
         //space optimization
         vector<int> v(n);
         //base case initialization
