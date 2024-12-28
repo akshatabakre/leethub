b09e624bf1d5e7e7bcc6856b3c6acc95d87b9a46
Akshata Bakre <148846716+akshatabakre@users.noreply.github.com>
2024-12-28 11:29:09 +0530

    Time: 11 ms (91.54%), Space: 8.6 MB (92.13%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -22,25 +22,25 @@ public:
         // vector<vector<int>> dp(n,vector<int>(m,-1));
         // return func(n-1,m-1,s,t,dp);
         //TABULATION
-        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
-        for(int i=0;i<n;i++)    dp[i][0] = 1;
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=m;j++){
-                if(s[i-1]==t[j-1]){
-                    dp[i][j] = (dp[i-1][j-1] + dp[i-1][j])%mod;
-                }else{
-                    dp[i][j] = dp[i-1][j]%mod;
-                }
-            }
-        }
-        return dp[n][m];
+        // vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+        // for(int i=0;i<n;i++)    dp[i][0] = 1;
+        // for(int i=1;i<=n;i++){
+        //     for(int j=1;j<=m;j++){
+        //         if(s[i-1]==t[j-1]){
+        //             dp[i][j] = (dp[i-1][j-1] + dp[i-1][j])%mod;
+        //         }else{
+        //             dp[i][j] = dp[i-1][j]%mod;
+        //         }
+        //     }
+        // }
+        // return dp[n][m];
         // SPACE OPTIMIZATION
         vector<int> v(m+1,0);
         v[0] = 1;
         for(int i=1;i<=n;i++){
-            for(int j=1;j<=m;j++){
+            for(int j=m;j>=1;j--){
                 if(s[i-1]==t[j-1]){
-                    v[j]+=v[j-1];
+                    v[j]=(v[j]+v[j-1])%mod;
                 }
             }
         }
