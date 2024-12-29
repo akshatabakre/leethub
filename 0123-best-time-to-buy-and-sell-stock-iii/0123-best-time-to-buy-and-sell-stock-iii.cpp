0891c7f43574b6041199a1a9d499c6781acc9332
Akshata Bakre <148846716+akshatabakre@users.noreply.github.com>
2024-12-29 23:05:33 +0530

    Time: 411 ms (34.44%), Space: 211.5 MB (34.28%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
-    int akshata(int ind,int canbuy,int cap,vector<int>& prices,vector<vector<vector<int>>>& dp){
-        if(cap==0){
-            return 0;
-        }
-        if(ind==prices.size()){
-            return 0;
-        }
-        if(dp[ind][canbuy][cap]!=-1){
-            return dp[ind][canbuy][cap];
-        }
-        if(canbuy){
-            return dp[ind][canbuy][cap] = max(-prices[ind]+akshata(ind+1,0,cap,prices,dp),akshata(ind+1,1,cap,prices,dp));
-        }else{
-            return dp[ind][canbuy][cap] = max(prices[ind]+akshata(ind+1,1,cap-1,prices,dp),akshata(ind+1,0,cap,prices,dp));
-        }
-    }
+    // int akshata(int ind,int canbuy,int cap,vector<int>& prices,vector<vector<vector<int>>>& dp){
+    //     if(cap==0){
+    //         return 0;
+    //     }
+    //     if(ind==prices.size()){
+    //         return 0;
+    //     }
+    //     if(dp[ind][canbuy][cap]!=-1){
+    //         return dp[ind][canbuy][cap];
+    //     }
+    //     if(canbuy){
+    //         return dp[ind][canbuy][cap] = max(-prices[ind]+akshata(ind+1,0,cap,prices,dp),akshata(ind+1,1,cap,prices,dp));
+    //     }else{
+    //         return dp[ind][canbuy][cap] = max(prices[ind]+akshata(ind+1,1,cap-1,prices,dp),akshata(ind+1,0,cap,prices,dp));
+    //     }
+    // }
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
         vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(2,vector<int>(3,0)));
