3b49b8154bc19775d33f9e61bfdac557e1866a9d
Akshata Bakre <148846716+akshatabakre@users.noreply.github.com>
2024-12-30 12:42:19 +0530

    Time: 146 ms (53.73%), Space: 139.6 MB (50.9%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,21 +1,31 @@
 class Solution {
 public:
-    int solve(int ind,int transaction,vector<int>& prices,vector<vector<int>>& dp){
-        if(ind == prices.size() || transaction==4){
-            return 0;
-        }
-        if(dp[ind][transaction]!=-1){
-            return dp[ind][transaction];
-        }
-        if(transaction%2==0){
-            return dp[ind][transaction] = max(-prices[ind]+solve(ind+1,transaction+1,prices,dp),solve(ind+1,transaction,prices,dp));
-        }else{
-            return dp[ind][transaction] = max(prices[ind]+solve(ind+1,transaction+1,prices,dp),solve(ind+1,transaction,prices,dp));
-        }
-    }
+    // int solve(int ind,int transaction,vector<int>& prices,vector<vector<int>>& dp){
+    //     if(ind == prices.size() || transaction==4){
+    //         return 0;
+    //     }
+    //     if(dp[ind][transaction]!=-1){
+    //         return dp[ind][transaction];
+    //     }
+    //     if(transaction%2==0){
+    //         return dp[ind][transaction] = max(-prices[ind]+solve(ind+1,transaction+1,prices,dp),solve(ind+1,transaction,prices,dp));
+    //     }else{
+    //         return dp[ind][transaction] = max(prices[ind]+solve(ind+1,transaction+1,prices,dp),solve(ind+1,transaction,prices,dp));
+    //     }
+    // }
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<int>> dp(n,vector<int>(4,-1));
-        return solve(0,0,prices,dp);
+        vector<vector<int>> dp(n+1,vector<int>(5,0));
+        // return solve(0,0,prices,dp);
+        for(int ind=n-1;ind>=0;ind--){
+            for(int t=0;t<4;t++){
+                if(t%2==0){
+                    dp[ind][t] = max(dp[ind+1][t+1]-prices[ind],dp[ind+1][t]);
+                }else{
+                    dp[ind][t] = max(dp[ind+1][t+1]+prices[ind],dp[ind+1][t]);
+                }
+            }
+        }
+        return dp[0][0];
     }
 };
