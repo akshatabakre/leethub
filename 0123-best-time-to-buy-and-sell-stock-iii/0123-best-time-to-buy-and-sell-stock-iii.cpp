5b05ef01f9f8a5a3326a534e7cdc4a10c929f6c8
Akshata Bakre <148846716+akshatabakre@users.noreply.github.com>
2024-12-30 12:39:12 +0530

    Time: 183 ms (50.47%), Space: 138.5 MB (52.09%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,40 +1,21 @@
 class Solution {
 public:
-    // int akshata(int ind,int canbuy,int cap,vector<int>& prices,vector<vector<vector<int>>>& dp){
-    //     if(cap==0){
-    //         return 0;
-    //     }
-    //     if(ind==prices.size()){
-    //         return 0;
-    //     }
-    //     if(dp[ind][canbuy][cap]!=-1){
-    //         return dp[ind][canbuy][cap];
-    //     }
-    //     if(canbuy){
-    //         return dp[ind][canbuy][cap] = max(-prices[ind]+akshata(ind+1,0,cap,prices,dp),akshata(ind+1,1,cap,prices,dp));
-    //     }else{
-    //         return dp[ind][canbuy][cap] = max(prices[ind]+akshata(ind+1,1,cap-1,prices,dp),akshata(ind+1,0,cap,prices,dp));
-    //     }
-    // }
+    int solve(int ind,int transaction,vector<int>& prices,vector<vector<int>>& dp){
+        if(ind == prices.size() || transaction==4){
+            return 0;
+        }
+        if(dp[ind][transaction]!=-1){
+            return dp[ind][transaction];
+        }
+        if(transaction%2==0){
+            return dp[ind][transaction] = max(-prices[ind]+solve(ind+1,transaction+1,prices,dp),solve(ind+1,transaction,prices,dp));
+        }else{
+            return dp[ind][transaction] = max(prices[ind]+solve(ind+1,transaction+1,prices,dp),solve(ind+1,transaction,prices,dp));
+        }
+    }
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        // vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(2,vector<int>(3,0)));
-        // return akshata(0,1,2,prices,dp);
-        //base cases -> for cap==0, dp = 0 && for ind==n, dp=0
-        //space optimization -> to a 2D DP
-        vector<vector<int>> v(2,vector<int>(3,0)),c(2,vector<int>(3,0));
-        for(int ind=n-1;ind>=0;ind--){
-            for(int buy=1;buy>=0;buy--){
-                for(int cap=1;cap<3;cap++){
-                    if(buy){
-                        c[buy][cap] = max(-prices[ind]+v[0][cap],v[1][cap]);
-                    }else{
-                        c[buy][cap] = max(prices[ind]+v[1][cap-1],v[0][cap]);
-                    }
-                }
-                v=c;
-            }
-        }
-        return v[1][2];
+        vector<vector<int>> dp(n,vector<int>(4,-1));
+        return solve(0,0,prices,dp);
     }
 };
