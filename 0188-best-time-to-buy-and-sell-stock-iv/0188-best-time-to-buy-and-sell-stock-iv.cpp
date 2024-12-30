79b9841bfca2d786c435fa7b484440a6964ce805
Akshata Bakre <148846716+akshatabakre@users.noreply.github.com>
2024-12-30 13:31:07 +0530

    Time: 12 ms (38.74%), Space: 17.9 MB (34.51%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,34 +1,20 @@
 class Solution {
 public:
-    int solve(int ind,int buy,int cap,vector<int>& prices,vector<vector<vector<int>>>& dp){
-        if(ind==prices.size()||cap==0){
+    int solve(int ind, int tr,int& maxtr,vector<int>& pr,vector<vector<int>>& dp){
+        if(ind==pr.size()||tr==maxtr){
             return 0;
         }
-        if(dp[ind][buy][cap]!=-1){
-            return dp[ind][buy][cap];
+        if(dp[ind][tr]!=-1){
+            return dp[ind][tr];
         }
-        if(buy){
-            return dp[ind][buy][cap] = max(-prices[ind]+solve(ind+1,0,cap,prices,dp),solve(ind+1,1,cap,prices,dp));
+        if(tr%2==0){
+            return dp[ind][tr] = max(-pr[ind]+solve(ind+1,tr+1,maxtr,pr,dp),solve(ind+1,tr,maxtr,pr,dp));
         }
-        return dp[ind][buy][cap] = max(prices[ind]+solve(ind+1,1,cap-1,prices,dp),solve(ind+1,0,cap,prices,dp));
+        return dp[ind][tr] = max(pr[ind]+solve(ind+1,tr+1,maxtr,pr,dp),solve(ind+1,tr,maxtr,pr,dp));
     }
     int maxProfit(int k, vector<int>& prices) {
-        int n = prices.size();
-        // vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(2,vector<int>(k+1,0)));
-        vector<vector<int>> v(2,vector<int>(k+1,0)),cur(2,vector<int>(k+1,0));
-        // return solve(0,1,k,prices,dp);
-        for(int i=n-1;i>=0;i--){
-            for(int b=0;b<2;b++){
-                for(int c=1;c<k+1;c++){
-                    if(b==1){
-                        cur[b][c] = max(-prices[i]+v[0][c],v[1][c]);
-                    }else{
-                        cur[b][c] = max(prices[i]+v[1][c-1],v[0][c]);
-                    }
-                }
-            }
-            v=cur;
-        }
-        return v[1][k];
+        int maxtr = 2*k, n = prices.size();;
+        vector<vector<int>> dp(n,vector<int>(maxtr,-1));
+        return solve(0,0,maxtr,prices,dp);
     }
 };
