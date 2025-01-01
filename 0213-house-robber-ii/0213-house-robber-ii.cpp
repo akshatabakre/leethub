c1b7c2fbae37122ee2885ba9a1dac1b8c037e24e
Akshata Bakre <148846716+akshatabakre@users.noreply.github.com>
2025-01-01 13:15:47 +0530

    Time: 22 ms (1.02%), Space: 11.1 MB (7.08%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,35 +1,34 @@
 class Solution {
 public:
-    int solve(vector<int>& nums){
-        int prev = nums[0], prev2 = 0, curr = 0;
-        for(int i=1;i<nums.size();i++){
-            int pick = nums[i],notpick = 0;
-            if(i>1){
-                pick += prev2;
-            }
-            notpick += prev;
-            curr = max(pick,notpick);
-            prev2 = prev;
-            prev = curr;
+     int solve(vector<int>& nums,int index,vector<vector<int>> & dp,int flag){
+        cout<<index<<" "<<flag<<endl;
+        if(index>=nums.size()){
+            return 0;
         }
-        return prev;
+        if(index==nums.size()-1 && flag==0){
+            return nums[index];
+        }
+        if(index==nums.size()-1 && flag==1){
+            return 0;
+        }
+        if(dp[index][flag]!=-1){
+            return dp[index][flag];
+        }
+        int include = 0,exclude = 0;
+        if(index==0){
+            include = nums[index]+solve(nums,index+2,dp,true);
+            exclude = solve(nums,index+1,dp,false);
+        }else{
+            include = nums[index]+solve(nums,index+2,dp,flag);
+            exclude = solve(nums,index+1,dp,flag);
+        }
+        dp[index][flag]=max(include,exclude);
+        return dp[index][flag];
     }
     int rob(vector<int>& nums) {
-        if(nums.size()==1)  return nums[0];
-        vector<int> v1, v2;
-        for(int i=0;i<nums.size();i++){
-            if(i!=0){
-                v2.push_back(nums[i]);
-            }
-            if(i!=nums.size()-1){
-                v1.push_back(nums[i]);
-            }
-        }
-        for(int i=0;i<v1.size();i++){
-            cout<<v2[i]<<" ";
-        }
-        cout<<endl;
-        cout<<solve(v1)<<" "<<solve(v2)<<endl;
-        return max(solve(v1),solve(v2));
+        // bool flag=false;
+        vector<vector<int>> dp(nums.size(),vector<int>(2,-1));
+        int ans=solve(nums,0,dp,0);
+        return ans;
     }
 };
