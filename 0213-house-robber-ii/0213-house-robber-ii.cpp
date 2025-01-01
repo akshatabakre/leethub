8465fc388b39afe07f332dd12f5b5b2b6e8edff7
Akshata Bakre <148846716+akshatabakre@users.noreply.github.com>
2025-01-01 13:20:54 +0530

    Time: 0 ms (100%), Space: 11.2 MB (7.08%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
-     int solve(vector<int>& nums,int index,vector<vector<int>> & dp,int flag){
-        cout<<index<<" "<<flag<<endl;
+     int solve(vector<int>& nums,int index,vector<vector<int>>& dp,int  flag){
         if(index>=nums.size()){
             return 0;
         }
@@ -14,21 +13,21 @@ public:
         if(dp[index][flag]!=-1){
             return dp[index][flag];
         }
-        int include = 0,exclude = 0;
         if(index==0){
-            include = nums[index]+solve(nums,index+2,dp,true);
-            exclude = solve(nums,index+1,dp,false);
-        }else{
-            include = nums[index]+solve(nums,index+2,dp,flag);
-            exclude = solve(nums,index+1,dp,flag);
+            flag=1;
         }
+        int include=solve(nums,index+2,dp,flag)+nums[index];
+        if(index==0){
+            flag=0;
+        }
+        int exclude=solve(nums,index+1,dp,flag);
         dp[index][flag]=max(include,exclude);
         return dp[index][flag];
     }
     int rob(vector<int>& nums) {
-        // bool flag=false;
+        bool flag=false;
         vector<vector<int>> dp(nums.size(),vector<int>(2,-1));
-        int ans=solve(nums,0,dp,0);
+        int ans=solve(nums,0,dp,flag);
         return ans;
     }
 };
