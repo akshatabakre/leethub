1fec904ae8e2b5e0a733a70864bd569d2ca23add
Akshata Bakre <148846716+akshatabakre@users.noreply.github.com>
2025-01-02 14:24:32 +0530

    Time: 75 ms (49.84%), Space: 14.7 MB (34.78%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -4,14 +4,35 @@ public:
         int n = nums.size();
         int ans = 0;
         vector<int> dp(n,1);
+        vector<int> hash(n,0);
+        for(int i=0;i<n;i++)    hash[i] = i;
+        int lastin = n;
         for(int i=0;i<n;i++){
             for(int j=0;j<i;j++){
                 if(nums[j]<nums[i]){
-                    dp[i] = max(dp[i],1+dp[j]);
+                    // dp[i] = max(dp[i],1+dp[j]);
+                    if(dp[i]<1+dp[j]){
+                        dp[i] = 1+dp[j];
+                        hash[i] = j;
+                    }
                 }
             }
-            ans = max(ans,dp[i]);
+            // ans = max(ans,dp[i]);
+            if(dp[i]>ans){
+                ans = dp[i];
+                lastin = i;
+            }
         }
-        return ans;
+        cout<<lastin<<endl;
+        vector<int> v;
+        v.push_back(nums[lastin]);
+        while(hash[lastin]!=lastin){
+            v.push_back(nums[hash[lastin]]);
+            lastin = hash[lastin];
+        }
+        // for(int i=0;i<hash.size();i++){
+        //     cout<<hash[i]<<" ";
+        // }
+        return v.size();
     }
 };
