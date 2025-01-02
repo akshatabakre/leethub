7a878dd5cad8b9e2b3b2d188222db8f6fb480fb4
Akshata Bakre <148846716+akshatabakre@users.noreply.github.com>
2025-01-02 16:19:40 +0530

    Time: 0 ms (100%), Space: 13.9 MB (70.77%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,38 +1,23 @@
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size();
-        int ans = 0;
-        vector<int> dp(n,1);
-        vector<int> hash(n,0);
-        for(int i=0;i<n;i++)    hash[i] = i;
-        int lastin = n;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<i;j++){
-                if(nums[j]<nums[i]){
-                    // dp[i] = max(dp[i],1+dp[j]);
-                    if(dp[i]<1+dp[j]){
-                        dp[i] = 1+dp[j];
-                        hash[i] = j;
-                    }
-                }
-            }
-            // ans = max(ans,dp[i]);
-            if(dp[i]>ans){
-                ans = dp[i];
-                lastin = i;
-            }
-        }
-        cout<<lastin<<endl;
         vector<int> v;
-        v.push_back(nums[lastin]);
-        while(hash[lastin]!=lastin){
-            v.push_back(nums[hash[lastin]]);
-            lastin = hash[lastin];
+        v.push_back(nums[0]);
+        int len = 1;
+        for(int i=1;i<nums.size();i++){
+            if(nums[i]>v.back()){
+                v.push_back(nums[i]);
+                len++;
+                // cout<<"pushing "<<nums[i]<<endl;
+            }else{
+                int ind = lower_bound(v.begin(),v.end(),nums[i]) - v.begin();
+                v[ind] = nums[i];
+            }
         }
-        // for(int i=0;i<hash.size();i++){
-        //     cout<<hash[i]<<" ";
+        // for(int i=0;i<v.size();i++){
+        //     cout<<v[i]<<" ";
         // }
-        return v.size();
+        // cout<<endl;
+        return len;
     }
 };
