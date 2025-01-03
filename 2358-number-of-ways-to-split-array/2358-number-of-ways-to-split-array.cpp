80912bce835023b378e1ca03c42a7dba5a23a126
Akshata Bakre <148846716+akshatabakre@users.noreply.github.com>
2025-01-03 11:10:56 +0530

    Time: 6 ms (30.21%), Space: 97 MB (26.39%) - LeetHub

--- 2358-number-of-ways-to-split-array/2358-number-of-ways-to-split-array.cpp
+++ 2358-number-of-ways-to-split-array/2358-number-of-ways-to-split-array.cpp
@@ -1,21 +1,21 @@
-#define ll long long
 class Solution {
 public:
     int waysToSplitArray(vector<int>& nums) {
-        ll sum1 = nums[0], sum2 = 0, n = nums.size();
-        for(int i=1;i<n;i++){
-            sum2+=nums[i];
-        }
-        int ans = 0;
-        if(sum1>=sum2){
-            ans++;
-        }
-        for(int i=1;i<n-1;i++){
-            sum1+=(2*nums[i]);
-            if(sum1>=sum2){
-                ans++;
+        long long totsum=0,n=nums.size();
+        vector<long long> prefix(n);
+        prefix[0] = nums[0];
+        for(int i=0;i<n;i++){
+            totsum+=nums[i];
+            if(i!=0){
+                prefix[i] = prefix[i-1] + nums[i];
             }
         }
-        return ans;
+        int valid = 0;
+        for(int i=0;i<n-1;i++){
+            if(2*prefix[i]>=totsum){
+                valid++;
+            }
+        }
+        return valid;
     }
 };
