7746b9d1065b7669c1891cc91dc52dbce8d80397
Akshata Bakre <148846716+akshatabakre@users.noreply.github.com>
2025-01-17 16:31:31 +0530

    Time: 0 ms (100%), Space: 10.8 MB (60.47%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -11,15 +11,33 @@
  */
 class Solution {
 public:
-    void inord(TreeNode* n,vector<int>& ans){
-        if(n==NULL)     return;
-        inord(n->left,ans);
-        ans.push_back(n->val);
-        inord(n->right,ans);
-    }
+    // void inord(TreeNode* n,vector<int>& ans){
+    //     if(n==NULL)     return;
+    //     inord(n->left,ans);
+    //     ans.push_back(n->val);
+    //     inord(n->right,ans);
+    // }
     vector<int> inorderTraversal(TreeNode* root) {
         vector<int> ans;
-        inord(root,ans);
+        // inord(root,ans);
+        // return ans;
+        if(!root){
+            return ans;
+        }
+        stack<TreeNode*> s;
+        TreeNode* n = root;
+        while(true){
+            if(n!=NULL){
+                s.push(n);
+                n = n->left;
+            }else{//null node encountered
+                if(s.empty())   break;
+                n = s.top();//re-initializing n to top of stack, as cant traverse the tree more (as the NULL node is encountered), basically backtracking
+                s.pop();
+                ans.push_back(n->val);
+                n = n->right;
+            }
+        }
         return ans;
     }
 };
