d6d1910d7a0a3a495071b40be5b753d092c88bdf
Akshata Bakre <148846716+akshatabakre@users.noreply.github.com>
2025-01-17 16:00:35 +0530

    Time: 0 ms (100%), Space: 11.1 MB (13.61%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
@@ -11,17 +11,32 @@
  */
 class Solution {
 public:
-    void preord(TreeNode* node,vector<int>& ans){
-        if(node==NULL){
-            return;
-        }
-        ans.push_back(node->val);
-        preord(node->left,ans);
-        preord(node->right,ans);
-    }
+    // void preord(TreeNode* node,vector<int>& ans){
+    //     if(node==NULL){
+    //         return;
+    //     }
+    //     ans.push_back(node->val);
+    //     preord(node->left,ans);
+    //     preord(node->right,ans);
+    // }
     vector<int> preorderTraversal(TreeNode* root) {
         vector<int> ans;
-        preord(root,ans);
+        // preord(root,ans);
+        // return ans;
+        if(root==NULL){
+            return ans;
+        }
+        stack<TreeNode*> s;
+        s.push(root);
+        while(!s.empty()){
+            TreeNode* n = s.top();
+            ans.push_back(n->val);
+            s.pop();
+            if(n->right!=NULL)
+                s.push(n->right);
+            if(n->left!=NULL)
+                s.push(n->left);
+        }
         return ans;
     }
 };
