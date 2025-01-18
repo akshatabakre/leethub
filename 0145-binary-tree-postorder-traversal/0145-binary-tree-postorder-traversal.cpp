d0814abb23082503f4ed9fc72bbddc516291be85
Akshata Bakre <148846716+akshatabakre@users.noreply.github.com>
2025-01-18 11:03:28 +0530

    Time: 0 ms (100%), Space: 11 MB (41.06%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
@@ -21,25 +21,51 @@ public:
     // }
     vector<int> postorderTraversal(TreeNode* root) {
         vector<int> ans;
+        //recursion
         // postord(root,ans);
-        stack<TreeNode*> st1;
+        //using two stacks
+        // stack<TreeNode*> st1;
         if(!root)   return ans;
-        st1.push(root);
-        while(!st1.empty()){
-            TreeNode* node = st1.top();
-            st1.pop();
-            if(node->left!=NULL)
-                st1.push(node->left);
-            if(node->right!=NULL)
-                st1.push(node->right);
-            ans.push_back(node->val);
-        }
-        // while(!st2.empty()){
-        //     TreeNode* n = st2.top();
-        //     ans.push_back(n->val);
-        //     st2.pop();
+        // st1.push(root);
+        // while(!st1.empty()){
+        //     TreeNode* node = st1.top();
+        //     st1.pop();
+        //     if(node->left!=NULL)
+        //         st1.push(node->left);
+        //     if(node->right!=NULL)
+        //         st1.push(node->right);
+        //     ans.push_back(node->val);
         // }
-        reverse(ans.begin(),ans.end());
+        // // while(!st2.empty()){
+        // //     TreeNode* n = st2.top();
+        // //     ans.push_back(n->val);
+        // //     st2.pop();
+        // // }
+        // reverse(ans.begin(),ans.end());
+        //one stack
+        stack<TreeNode*> st;
+        TreeNode* curr = root;
+        while(curr!=NULL || !st.empty()){
+            if(curr!=NULL){
+                st.push(curr);
+                curr = curr->left;
+            }else{//no left of prev curr
+                TreeNode* temp = st.top()->right;
+                if(temp==NULL){//leaf node reached
+                    temp = st.top();
+                    st.pop();
+                    ans.push_back(temp->val);
+                    while(!st.empty() && temp==st.top()->right){
+                        //adding root, after right
+                        temp = st.top();
+                        st.pop();
+                        ans.push_back(temp->val);
+                    }
+                }else{
+                    curr = temp;//go to right child
+                }
+            }
+        }
         return ans;
     }
 };
