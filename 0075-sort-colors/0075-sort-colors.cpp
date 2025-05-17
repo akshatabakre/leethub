f790926963f76ee81965d91ea2793ee147d09d8b
Akshata Bakre <148846716+akshatabakre@users.noreply.github.com>
2025-05-17 21:05:29 +0530

    Time: 0 ms (100%), Space: 11.6 MB (48.91%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,23 +1,17 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int r=0,w=0;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]==0){
-                r++;
-            }else if(nums[i]==1){
-                w++;
+        map<int,int> m;
+        int n = nums.size();
+        for(int i=0;i<n;i++){
+            m[nums[i]]++;
+        }
+        int ind = 0;
+        for(auto i:m){
+            for(int j=0;j<i.second;j++){
+                nums[ind++] = i.first;
             }
         }
-        int j = 0;
-        while(r--){
-            nums[j++] = 0;
-        }
-        while(w--){
-            nums[j++] = 1;
-        }
-        while(j!=nums.size()){
-            nums[j++] = 2;
-        }
+        return;
     }
 };
