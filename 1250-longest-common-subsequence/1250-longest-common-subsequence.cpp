bba1640d5a4b51231ec7cf7260651317932c16fa
Akshata Bakre <148846716+akshatabakre@users.noreply.github.com>
2025-06-10 23:12:12 +0530

    Time: 23 ms (80.05%), Space: 27.6 MB (28.01%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
@@ -1,42 +1,38 @@
 class Solution {
 public:
-    // int lcs(int ind1,int ind2,string text1,string text2,vector<vector<int>>& dp){
-    //     if(ind1<0||ind2<0){
-    //         return 0;
-    //     }
-    //     if(dp[ind1][ind2]!=-1){
-    //         return dp[ind1][ind2];
-    //     }
-    //     if(text1[ind1]==text2[ind2]){
-    //         return dp[ind1][ind2] = 1+lcs(ind1-1,ind2-1,text1,text2,dp);
-    //     }
-    //     return dp[ind1][ind2] = max(lcs(ind1-1,ind2,text1,text2,dp),lcs(ind1,ind2-1,text1,text2,dp));
-    // }
     int longestCommonSubsequence(string text1, string text2) {
-        int n=text1.length(),m=text2.length();
-        // vector<vector<int>> dp(n,vector<int>(m,0));
-        // return lcs(n-1,m-1,text1,text2,dp);
-        vector<int> prev(m,0),curr(m,0);
-        for(int i1=0;i1<n;i1++){
-            for(int i2=0;i2<m;i2++){
-                if(text1[i1]==text2[i2]){
-                    curr[i2] = 1;
-                    if(i1>0 && i2>0){
-                        curr[i2]+=prev[i2-1];
-                    }
+        int n = text1.length(), m = text2.length();
+        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+        dp[0][0] = 0;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(text1[i-1]==text2[j-1]){
+                    dp[i][j] = 1 + dp[i-1][j-1];
                 }else{
-                    int v1=0,v2=0;
-                    if(i1>0){
-                        v1=prev[i2];
-                    }
-                    if(i2>0){
-                        v2=curr[i2-1];
-                    }
-                    curr[i2] = max(v1,v2);
+                    dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
                 }
             }
-            prev = curr;
         }
-        return prev[m-1];
+        //index i in dp - i-1 in string
+        int len = dp[n][m];
+        int i=n,j=m;
+        string str = "";
+        for(int i=0;i<len;i++){
+            str+='$';
+        }
+        int index = len-1;
+        while(i>0 && j>0){
+            if(text1[i-1]==text2[j-1]){
+                str[index] = text1[i-1];
+                index--;
+                i--;    j--;
+            }else if(text1[i-1]>text2[j-1]){
+                i--;
+            }else{
+                j--;
+            }
+        }
+        cout<<str;
+        return len;
     }
 };
