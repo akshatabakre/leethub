26c1384d3466a83ac593d727194321a9ea356992
Akshata Bakre <148846716+akshatabakre@users.noreply.github.com>
2025-06-10 16:56:59 +0530

    Time: 0 ms (100%), Space: 9.2 MB (56.8%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,17 +1,24 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        vector<int> v(n,1);//i=0 base case
-        //all of the first row cells will have one path to go to 0,0
-        for(int i=1;i<m;i++){
-            vector<int> temp(n,0);
+        vector<vector<int>> dp(m,vector<int>(n,0));
+        for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                int up = v[j], left = 0;
-                if(j>0) left = temp[j-1];
-                temp[j] = up+left;
+                if(i==0 && j==0){
+                    dp[i][j] =1;
+                }else{
+                    int lft = 0, up = 0;
+                    if(i>0){
+                        up = dp[i-1][j];
+                    }
+                    if(j>0){
+                        lft = dp[i][j-1];
+                    }
+                    dp[i][j] = up + lft;
+                }
+
             }
-            v = temp;
         }
-        return v[n-1];
+        return dp[m-1][n-1];
     }
 };
