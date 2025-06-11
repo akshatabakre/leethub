48d7c4c4d7064f3b57cf28293aaba9ba2e707f7c
Akshata Bakre <148846716+akshatabakre@users.noreply.github.com>
2025-06-11 14:11:08 +0530

    Time: 543 ms (19.78%), Space: 296.5 MB (20.51%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -4,22 +4,22 @@ public:
         if(ind==nums.size()){
             return 0;
         }
-        if(prevind!=-1 && dp[ind][prevind]!=-1)    return dp[ind][prevind];
+        if(dp[ind][prevind]!=-1)    return dp[ind][prevind];
         int nottake = 0 + f(ind+1,prevind,nums,dp);
         int take = 0;
-        if(prevind==-1){
-            take = 1 + f(ind+1,ind,nums,dp);
-        } else if(nums[ind]>nums[prevind]){
-            take = 1 + f(ind+1,ind,nums,dp);
+        if(prevind==0){
+            take = 1 + f(ind+1,ind+1,nums,dp);
+        } else if(nums[ind]>nums[prevind-1]){
+            take = 1 + f(ind+1,ind+1,nums,dp);
         }
-        if(prevind==-1){
-            return max(take,nottake);
-        }
+        // if(prevind==-1){
+        //     return max(take,nottake);
+        // }
         return dp[ind][prevind] = max(take,nottake);
     }
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
         vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
-        return f(0,-1,nums,dp);
+        return f(0,0,nums,dp);
     }
 };
