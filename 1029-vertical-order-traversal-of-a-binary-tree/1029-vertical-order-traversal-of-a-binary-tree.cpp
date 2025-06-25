810bf79040931a9665e014421316908566a1e6e0
Akshata Bakre <148846716+akshatabakre@users.noreply.github.com>
2025-06-25 19:30:02 +0530

    Time: 2 ms (50.7%), Space: 17.6 MB (5.01%) - LeetHub

--- 1029-vertical-order-traversal-of-a-binary-tree/1029-vertical-order-traversal-of-a-binary-tree.cpp
+++ 1029-vertical-order-traversal-of-a-binary-tree/1029-vertical-order-traversal-of-a-binary-tree.cpp
@@ -11,33 +11,32 @@
  */
 class Solution {
 public:
-    void traverse(TreeNode* root,int vert,int level,map<int,
-    map<int,multiset<int>>>& mp,int& mini,int& maxi){
-        if(!root)   return;
-        mp[vert][level].insert(root->val);
-        mini = min(vert,mini);
-        maxi = max(vert,maxi);
-        traverse(root->left,vert-1,level+1,mp,mini,maxi);
-        traverse(root->right,vert+1,level+1,mp,mini,maxi);
+    void solve(TreeNode* root,int v,int l,map<int,map<int,multiset<int>>>& m){
+        if(!root){
+            return;
+        }
+        m[v][l].insert(root->val);
+        solve(root->left,v-1,l+1,m);
+        solve(root->right,v+1,l+1,m);
     }
     vector<vector<int>> verticalTraversal(TreeNode* root) {
+        map<int,map<int,multiset<int>>> m;
+        solve(root,0,0,m);
         vector<vector<int>> ans;
-        if(!root)   return ans;
-        if(!root->left && !root->right){
-            ans.push_back({root->val});
-            return ans;
-        }
-        map<int,map<int,multiset<int>>> mp;
-        int mini = 1e9, maxi = -1e9;
-        traverse(root,0,0,mp,mini,maxi);
-        ans.resize(maxi-mini+1);
-        for(auto it1:mp){
-            for(auto it2:it1.second){
-                for(int i:it2.second){
-                    ans[it1.first-mini].push_back(i);
+        for(auto v:m){
+            int vert = v.first;
+            auto mp = v.second;
+            vector<int> vec;
+            for(auto l:mp){
+                int lev = l.first;
+                auto ms = l.second;
+                
+                for(int it:ms){
+                    vec.push_back(it);
                 }
             }
+                ans.push_back(vec);
         }
-        return ans;
+            return ans;
     }
 };
