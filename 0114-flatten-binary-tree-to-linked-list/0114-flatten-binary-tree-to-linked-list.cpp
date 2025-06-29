c2d1a88749cfc71e01daed83e5b4f1c7a6b3237e
Akshata Bakre <148846716+akshatabakre@users.noreply.github.com>
2025-06-29 13:23:27 +0530

    Time: 0 ms (100%), Space: 17.6 MB (49.93%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.cpp
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.cpp
@@ -11,38 +11,21 @@
  */
 class Solution {
 public:
-    void solve(TreeNode* root,TreeNode* &prev){
-        if(!root)   return;
-        solve(root->right,prev);
-        solve(root->left,prev);
-        root->right = prev;
-        root->left = NULL;
-        prev = root;
-        return;
-    }
     void flatten(TreeNode* root) {
-        if(!root || (!root->left && !root->right))   return;
-        // TreeNode* curr = root;
-        // while(curr){
-        //     if(curr->left==NULL){
-        //         curr = curr->right;
-        //     }else{
-        //         TreeNode* ptr = curr->left;
-        //         while(ptr->right!=NULL){
-        //             ptr = ptr->right;
-        //         }
-        //         ptr->right = curr->right;
-        //         TreeNode* lft = curr->left;
-        //         curr->left = NULL;
-        //         curr->right = lft;
-        //         curr = curr->right;
-        //     }
-        // }
-        // return;
-
-        //recursive
-        TreeNode* prev = NULL;
-        solve(root,prev);
+        if(!root)   return;
+        TreeNode* curr = root;
+        while(curr){
+            if(curr->left){
+                TreeNode* temp = curr->left;
+                while(temp->right){
+                    temp = temp->right;
+                }
+                temp->right = curr->right;
+                curr->right = curr->left;
+                curr->left = NULL;
+            }
+            curr = curr->right;
+        }
         return;
     }
 };
