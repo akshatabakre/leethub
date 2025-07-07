a643e00f38410b3d4f3aafc33732327088696ae1
Akshata Bakre <148846716+akshatabakre@users.noreply.github.com>
2025-07-07 19:33:51 +0530

    Time: 11 ms (16.45%), Space: 28.6 MB (38.32%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -1,44 +1,18 @@
 class Solution {
 public:
     vector<int> nextGreaterElements(vector<int>& nums) {
-        stack<int> s;
-        int maxind = -1, maxi = INT_MIN, n = nums.size();
-        for(int i=0;i<n;i++){
-            if(maxi<=nums[i]){
-                maxind = i;
-                maxi = nums[i];
+        stack<int> st;
+        int n = nums.size();
+        vector<int> ans(n,-1);
+        for(int i=2*n-1;i>=0;i--){
+            while(!st.empty() && st.top()<=nums[i%n]){
+                st.pop();
             }
+            if(i<n && !st.empty()){
+                ans[i] = st.top();
+            }
+            st.push(nums[i%n]);
         }
-        cout<<maxi<<" "<<maxind<<endl;
-        vector<int> result(n);
-        result[maxind] = -1;
-        s.push(maxi);
-        if(maxind>0){
-            for(int i=maxind-1;i>=0;i--){
-                while(!s.empty() && nums[i]>=s.top()){
-                    s.pop();
-                }
-                if(s.empty()){
-                    result[i] = -1;
-                    s.push(nums[i]);
-                }else{
-                    result[i] = s.top();
-                    s.push(nums[i]);
-                }
-                // cout<<result[i]<<" at "<<i<<endl;
-            }
-        }
-        for(int i=n-1;i>=maxind+1;i--){
-            while(!s.empty() && nums[i]>=s.top()){
-                s.pop();
-            }
-            if(!s.empty()){
-                result[i] = s.top();
-                s.push(nums[i]);
-            }else{
-                result[i] = -1;
-            }
-        }
-        return result;
+        return ans;
     }
 };
