2fb0366c8eec47ab68b7bc46181afb7483b12ee3
Akshata Bakre <148846716+akshatabakre@users.noreply.github.com>
2025-07-09 12:03:02 +0530

    Time: 23 ms (23.32%), Space: 48.2 MB (20.04%) - LeetHub

--- 0943-sum-of-subarray-minimums/0943-sum-of-subarray-minimums.cpp
+++ 0943-sum-of-subarray-minimums/0943-sum-of-subarray-minimums.cpp
@@ -7,7 +7,7 @@ public:
         int n = arr.size();
         vector<int> left(n,-1),right(n,n);
         for(int i=0;i<n;i++){
-            while(!st.empty() && st.top().first>=arr[i]){
+            while(!st.empty() && st.top().first>arr[i]){
                 st.pop();
             }
             if(!st.empty()){
@@ -17,7 +17,7 @@ public:
         }
         while(!st.empty())  st.pop();
         for(int i=n-1;i>=0;i--){
-            while(!st.empty() && st.top().first>arr[i]){
+            while(!st.empty() && st.top().first>=arr[i]){
                 st.pop();
             }
             if(!st.empty()){
