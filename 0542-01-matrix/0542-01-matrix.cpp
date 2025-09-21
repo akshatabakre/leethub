4bfecb71741196977dc2f4601ecf632c24cea8c0
Akshata Bakre <148846716+akshatabakre@users.noreply.github.com>
2025-09-21 20:37:18 +0530

    Time: 106 ms (7.11%), Space: 62.8 MB (5.07%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,50 +1,33 @@
 class Solution {
 public:
+    bool isvalid(int a,int b,int m,int n){
+        return (a>=0 && b>=0 && a<m && b<n);
+    }
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
         int m = mat.size(), n = mat[0].size();
         vector<vector<int>> vis(m,vector<int>(n,0));
         vector<vector<int>> ans(m,vector<int>(n,0));
-        queue<pair<pair<int,int>,int>> q;
+        queue<vector<int>> q;//x,y,d
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(mat[i][j]==0){
+                    q.push({i,j,0});
                     vis[i][j] = 1;
                     ans[i][j] = 0;
-                    q.push({{i,j},0});
                 }
             }
         }
-        //do BFS
+        int dx[] = {-1,0,1,0}, dy[] = {0,-1,0,1};
         while(!q.empty()){
-            int r = q.front().first.first, c = q.front().first.second, t = q.front().second;
+            auto it = q.front();
             q.pop();
-            //check for 4 directions
-            if(r-1>=0 && r-1<m){
-                if(!vis[r-1][c]){
-                    vis[r-1][c] = 1;
-                    ans[r-1][c] = t+1;
-                    q.push({{r-1,c},t+1});
-                }
-            }
-            if(r+1>=0 && r+1<m){
-                if(!vis[r+1][c]){
-                    vis[r+1][c] = 1;
-                    ans[r+1][c] = t+1;
-                    q.push({{r+1,c},t+1});
-                }
-            }
-            if(c-1>=0 && c-1<n){
-                if(!vis[r][c-1]){
-                    vis[r][c-1] = 1;
-                    ans[r][c-1] = t+1;
-                    q.push({{r,c-1},t+1});
-                }
-            }
-            if(c+1>=0 && c+1<n){
-                if(!vis[r][c+1]){
-                    vis[r][c+1] = 1;
-                    ans[r][c+1] = t+1;
-                    q.push({{r,c+1},t+1});
+            int x = it[0], y = it[1], d = it[2];
+            for(int i=0;i<4;i++){
+                int nx = x + dx[i], ny = y + dy[i];
+                if(isvalid(nx,ny,m,n) && !vis[nx][ny]){
+                    q.push({nx,ny,d+1});
+                    ans[nx][ny] = d+1;
+                    vis[nx][ny] = 1;
                 }
             }
         }
