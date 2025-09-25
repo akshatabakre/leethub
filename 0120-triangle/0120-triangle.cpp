9131b7e44268c762fc613a94dd134429d67f7d72
Akshata Bakre <148846716+akshatabakre@users.noreply.github.com>
2025-09-25 13:43:30 +0530

    Time: 0 ms (100%), Space: 13.9 MB (5.06%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,45 +1,21 @@
 class Solution {
 public:
-    // int f(int i,int j,vector<vector<int>>& t,int n,vector<vector<int>>& dp){
-    //     if(i==n-1){
-    //         return t[n-1][j];
-    //     }
-    //     if(dp[i][j]!=-1){
-    //         return dp[i][j];
-    //     }
-    //     int d = f(i+1,j,t,n,dp);
-    //     int dg = f(i+1,j+1,t,n,dp);
-    //     return dp[i][j] = t[i][j] + min(d,dg);
-    // }
-    int minimumTotal(vector<vector<int>>& triangle) {
-        int n = triangle.size();
-        // vector<vector<int>> dp(n,vector<int> (n,-1));
-        // return f(0,0,triangle,n,dp);
-
-        //tabulation
-        //base case implementation
-        // for(int i=0;i<n;i++){
-        //     dp[n-1][i] = triangle[n-1][i];
-        // }
-        // for(int i=n-2;i>=0;i--){//row
-        //     for(int j=0;j<=i;j++){
-        //         dp[i][j] = triangle[i][j] + min(dp[i+1][j],dp[i+1][j+1]);
-        //     }
-        // }
-        // return dp[0][0];
-
-        //space optimization
-        vector<int> v(n);
-        //base case implementation
-        for(int i=0;i<n;i++)
-            v[i] = triangle[n-1][i];
-        for(int i=n-2;i>=0;i--){
-            vector<int> temp(n);
-            for(int j=0;j<=i;j++){
-                temp[j] = triangle[i][j] + min(v[j],v[j+1]);
-            }
-            v = temp;
+vector<vector<int>> dp;
+    vector<vector<int>> triangle;
+    int r;
+    int solve(int row,int col){
+        if(row==r){
+            return 0;
         }
-        return v[0];
+        if(dp[row][col]!=1e9){
+            return dp[row][col];
+        }
+        return dp[row][col] = triangle[row][col]+min(solve(row+1,col),solve(row+1,col+1));
+    }
+    int minimumTotal(vector<vector<int>>& triangle) {
+        this->r = triangle.size();
+        this->triangle = triangle;
+        dp.resize(r,vector<int>(r,1e9));
+        return solve(0,0);
     }
 };
