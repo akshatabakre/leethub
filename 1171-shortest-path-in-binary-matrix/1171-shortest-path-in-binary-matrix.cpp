f1521b2f7c2921d8b693f590a8a07d2308ce1e39
Akshata Bakre <148846716+akshatabakre@users.noreply.github.com>
2025-09-26 14:23:52 +0530

    Time: 48 ms (19.98%), Space: 36.7 MB (16.24%) - LeetHub

--- 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.cpp
@@ -1,34 +1,31 @@
 class Solution {
 public:
-    bool isValid(int r,int c,int n){
-        return (r>=0 && c>=0 && r<n && c<n);
+    bool isvalid(int nx,int ny,int n){
+        return (nx>=0 && ny>=0 && nx<n && ny<n);
     }
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        int dx[] = {0,1,1,1,0,-1,-1,-1};
-        int dy[] = {1,1,0,-1,-1,-1,0,1};
         int n = grid.size();
-        vector<vector<int>> D(n,vector<int>(n,1e9));
-        if(!grid[0][0])     D[0][0] = 0;
-        priority_queue<
-        pair<int,pair<int,int>>,
-        vector<pair<int,pair<int,int>>>,
-        greater<pair<int,pair<int,int>>>
-        > pq;
-        if(!grid[0][0])     pq.push({0,{0,0}});
-        while(!pq.empty()){
-            int x = pq.top().second.first;
-            int y = pq.top().second.second;
-            int d = pq.top().first;
-            pq.pop();
+        vector<vector<int>> dist(n,vector<int>(n,1e9));
+        int dx[] = {-1,-1,-1,0,0,1,1,1}, dy[] = {-1,0,1,-1,1,-1,0,1};
+        queue<vector<int>> q;//x,y,d
+        if(grid[0][0]==0){
+            q.push({0,0,1});
+            dist[0][0] = 1;
+        }
+        while(!q.empty()){
+            int x = q.front()[0], y = q.front()[1], d = q.front()[2];
+            q.pop();
             for(int i=0;i<8;i++){
                 int nx = x + dx[i], ny = y + dy[i];
-                if(isValid(nx,ny,n) && !grid[nx][ny] && 1+d<D[nx][ny]){
-                    D[nx][ny] = 1 + d;
-                    pq.push({1+d,{nx,ny}});
+                if(isvalid(nx,ny,n) && grid[nx][ny]==0 && dist[nx][ny]>1+d){
+                    dist[nx][ny] = 1 + d;
+                    q.push({nx,ny,d+1});
                 }
             }
         }
-        if(D[n-1][n-1]==1e9)    return -1;
-        return D[n-1][n-1]+1;
+        if(dist[n-1][n-1]==1e9){
+            return -1;
+        }
+        return dist[n-1][n-1];
     }
 };
