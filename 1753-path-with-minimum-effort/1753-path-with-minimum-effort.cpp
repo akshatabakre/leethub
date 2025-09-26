3f361a6e4668ed6adacc1436da9511c16bb355e8
Akshata Bakre <148846716+akshatabakre@users.noreply.github.com>
2025-09-26 14:34:55 +0530

    Time: 288 ms (6.29%), Space: 44.8 MB (18.11%) - LeetHub

--- 1753-path-with-minimum-effort/1753-path-with-minimum-effort.cpp
+++ 1753-path-with-minimum-effort/1753-path-with-minimum-effort.cpp
@@ -1,36 +1,27 @@
 class Solution {
 public:
-    bool isValid(int r,int c,int rows,int cols){
-        return (r>=0 && c>=0 && r<rows && c<cols);
+    bool isvalid(int nx,int ny,int row,int col){
+        return (nx>=0 && ny>=0 && nx<row && ny<col);
     }
     int minimumEffortPath(vector<vector<int>>& heights) {
-        int rows = heights.size(), cols = heights[0].size();
-        vector<vector<int>> minEffort(rows,vector<int>(cols,1e9));
-        minEffort[0][0] = 0;
-        priority_queue<
-        pair<int,pair<int,int>>,//effort,x,y
-        vector<pair<int,pair<int,int>>>,
-        greater<pair<int,pair<int,int>>>
-        > pq;
-        pq.push({0,{0,0}});
-        int dr[] = {0,1,0,-1};
-        int dc[] = {1,0,-1,0};
+        int row = heights.size(), col = heights[0].size();
+        vector<vector<int>> dist(row,vector<int>(col,1e9));
+        dist[0][0] = 0;
+        priority_queue<vector<int>,vector<vector<int>>,greater<vector<int>>> pq;
+        //x,y,d
+        pq.push({0,0,0});
+        int dx[] = {-1,0,1,0}, dy[] = {0,-1,0,1};
         while(!pq.empty()){
-            int eff = pq.top().first;
-            int r = pq.top().second.first;
-            int c = pq.top().second.second;
+            int x = pq.top()[0], y = pq.top()[1], d = pq.top()[2];
             pq.pop();
             for(int i=0;i<4;i++){
-                int nr = r + dr[i], nc = c + dc[i];
-                if(isValid(nr,nc,rows,cols)){
-                    int newEff = abs(heights[r][c]-heights[nr][nc]);
-                    if(max(eff,newEff) < minEffort[nr][nc]){
-                        minEffort[nr][nc] = max(eff,newEff);
-                        pq.push({minEffort[nr][nc],{nr,nc}});
-                    }
+                int nx = x + dx[i], ny = y + dy[i];
+                if(isvalid(nx,ny,row,col) && dist[nx][ny]>max(d,abs(heights[x][y]-heights[nx][ny]))){
+                    dist[nx][ny] = max(d,abs(heights[x][y]-heights[nx][ny]));
+                    pq.push({nx,ny,dist[nx][ny]});
                 }
             }
         }
-        return minEffort[rows-1][cols-1];
+        return dist[row-1][col-1];
     }
 };
