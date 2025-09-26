e8c28edf6b33a7d85b9509c7371d1f271e27205a
Akshata Bakre <148846716+akshatabakre@users.noreply.github.com>
2025-09-26 12:02:05 +0530

    Time: 53 ms (34.89%), Space: 65.2 MB (56.37%) - LeetHub

--- 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
+++ 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
@@ -1,36 +1,40 @@
 class Solution {
 public:
-    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        //using bfs (kahns algo/topological sort)
-        //step 1 - reverse edges
-        int n = graph.size();
-        vector<int> indegree(n,0),safe;
-        vector<vector<int>> reverse(n);
-        for(int i=0;i<graph.size();i++){
-            for(int j:graph[i]){
-                reverse[j].push_back(i);
-                indegree[i]++;
-            }
-        }
-        //step 2 - topo sort on reverse
-        queue<int> q;
-        for(int i=0;i<n;i++){
-            if(indegree[i]==0){
-                q.push(i);
-            }
-        }
-        while(!q.empty()){
-            int node = q.front();
-            q.pop();
-            safe.push_back(node);
-            for(int i:reverse[node]){
-                indegree[i]--;
-                if(indegree[i]==0){
-                    q.push(i);
+    map<int,bool> safe;
+    vector<vector<int>> graph;
+    vector<int> vis, pathvis;
+    bool dfs(int node){
+        vis[node] = 1;
+        pathvis[node] = 1;
+        for(auto adjnode:graph[node]){
+            if(!vis[adjnode]){
+                if(dfs(adjnode)){
+                    return true;
+                }
+            }else{
+                if(pathvis[adjnode]){
+                    return true;
                 }
             }
         }
-        sort(safe.begin(),safe.end());
-        return safe;
+        pathvis[node] = 0;
+        safe[node] = 1;//*
+        return false;
+    }
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        int n = graph.size();
+        this->graph = graph;
+        vis.resize(n,0);
+        pathvis.resize(n,0);
+        for(int i=0;i<n;i++){
+            if(!vis[i]){
+                dfs(i);//*
+            }
+        }
+        vector<int> safenodes;
+        for(auto it:safe){
+            safenodes.push_back(it.first);
+        }
+        return safenodes;
     }
 };
