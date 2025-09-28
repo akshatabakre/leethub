b7f824baf9beb394f7411ed322f0bc3d96dd345d
Akshata Bakre <148846716+akshatabakre@users.noreply.github.com>
2025-09-28 13:50:51 +0530

    Time: 0 ms (100%), Space: 13.1 MB (49.2%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,43 +1,24 @@
 class Solution {
 public:
-    // int minsteps(int i,int j,string& s,string& t,vector<vector<int>>& dp){
-    //     if(j<0){
-    //         return i+1;
-    //     }
-    //     if(i<0){
-    //         return j+1;
-    //     }
-    //     if(dp[i][j]!=-1){
-    //         return dp[i][j];
-    //     }
-    //     if(s[i]==t[j]){
-    //         return dp[i][j] = minsteps(i-1,j-1,s,t,dp);
-    //     }
-    //     return dp[i][j] = 1+min(minsteps(i-1,j-1,s,t,dp),min(minsteps(i-1,j,s,t,dp),minsteps(i,j-1,s,t,dp)));
-    // }
+    vector<vector<int>> dp;
+    int solve(int i,int j,string&s,string&t,int n1,int n2){
+        if(j==n2){
+            return (n1-i);
+        }
+        if(i==n1){
+            return (n2-j);
+        }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        if(s[i]==t[j]){
+            return dp[i][j] = solve(i+1,j+1,s,t,n1,n2);
+        }
+        return dp[i][j] = 1 + min(solve(i+1,j+1,s,t,n1,n2),min(solve(i+1,j,s,t,n1,n2),solve(i,j+1,s,t,n1,n2)));
+    }
     int minDistance(string word1, string word2) {
-        int n = word1.length(),m=word2.length();
-        // vector<vector<int>> dp(n,vector<int>(m,-1));
-        // return minsteps(n-1,m-1,word1,word2,dp);
-        // vector<vector<int>> dp(n+1,vector<int>(m+1,0));
-        vector<int> v(m+1,0),c(m+1,0);
-        for(int j=0;j<=m;j++){
-            v[j] = j;
-        }
-        // for(int i=0;i<=n;i++){
-        //     dp[i][0] = i;
-        // }
-        for(int i=1;i<=n;i++){
-            c[0] = i;
-            for(int j=1;j<=m;j++){
-                if(word1[i-1]==word2[j-1]){
-                    c[j] = v[j-1];
-                }else{
-                    c[j] = 1+min(v[j-1],min(v[j],c[j-1]));
-                }
-            }
-            v=c;
-        }
-        return v[m];
+        int n1 = word1.length(), n2 = word2.length();
+        dp.resize(n1,vector<int>(n2,-1));
+        return solve(0,0,word1,word2,n1,n2);
     }
 };
