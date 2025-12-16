8f55f1cbfc396117cc7544d3e66f929b9748b00f
Akshata Bakre <148846716+akshatabakre@users.noreply.github.com>
2025-12-17 01:04:14 +0530

    Time: 798 ms (71.46%), Space: 504.5 MB (24.83%) - LeetHub

---
+++ 3742-maximum-path-score-in-a-grid/3742-maximum-path-score-in-a-grid.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<vector<int>> grid;
+    vector<vector<vector<int>>> dp;
+    int solve(int i,int j,int k){
+        if(i>=grid.size() || j>=grid[0].size()){
+            return -1e9;
+        }
+        if(k<0){
+            return -1e9;
+        }
+        if(i==grid.size()-1 && j==grid[0].size()-1){
+            if(k==0){
+                if(grid[i][j]){
+                    return -1e9;
+                }
+                return 0;
+            }
+            return grid[grid.size()-1][grid[0].size()-1];
+        }
+        if(dp[i][j][k]!=-1){
+            return dp[i][j][k];
+        }
+        int c = 0;
+        if(grid[i][j]){
+            c = 1;
+        }
+        return dp[i][j][k] = grid[i][j] + max(solve(i+1,j,k-c),solve(i,j+1,k-c));
+    }
+    int maxPathScore(vector<vector<int>>& grid, int k) {
+        this->grid = grid;
+        int m = grid.size(), n = grid[0].size();
+        dp.resize(m,vector<vector<int>>(n,vector<int>(k+1,-1)));
+        int ans = solve(0,0,k);
+        if(ans<0){
+            return -1;
+        }
+        return ans;
+    }
+};
