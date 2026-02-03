870e890638ecea32f61eaf568fc4468725a72d04
Akshata Bakre <148846716+akshatabakre@users.noreply.github.com>
2026-02-03 18:16:02 +0530

    Time: 4 ms (7.38%), Space: 27.8 MB (10.15%) - LeetHub

---
+++ 3637-trionic-array-i/3637-trionic-array-i.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    bool isTrionic(vector<int>& nums) {
+        int n = nums.size();
+        // if(n<=3){
+        //     return false;
+        // }
+        for(int p=1;p<n-1;p++){
+            for(int q=p+1;q<n-1;q++){
+                bool flag = true;
+                for(int k=1;k<=p;k++){
+                    if(nums[k]<=nums[k-1]){
+                        flag = false;
+                        break;
+                    }
+                }
+                if(flag==false){
+                    continue;
+                }
+                for(int k=p+1;k<=q;k++){
+                    if(nums[k]>=nums[k-1]){
+                        flag = false;
+                        break;
+                    }
+                }
+                if(flag==false){
+                    continue;
+                }
+                for(int k=q+1;k<n;k++){
+                    if(nums[k]<=nums[k-1]){
+                        flag = false;
+                        break;
+                    }
+                }
+                if(flag==false){
+                    continue;
+                }
+                cout<<p<<q<<endl;
+                return true;
+            }
+        }
+        return false;
+    }
+};
