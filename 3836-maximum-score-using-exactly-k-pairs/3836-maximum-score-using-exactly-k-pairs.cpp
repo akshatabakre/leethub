bc64a98a1c4c9212103b6f09322618c7b9fc1f85
Akshata Bakre <148846716+akshatabakre@users.noreply.github.com>
2026-02-18 00:22:41 +0530

    Time: 1171 ms (30.13%), Space: 57.6 MB (91.14%) - LeetHub

--- 3836-maximum-score-using-exactly-k-pairs/3836-maximum-score-using-exactly-k-pairs.cpp
+++ 3836-maximum-score-using-exactly-k-pairs/3836-maximum-score-using-exactly-k-pairs.cpp
@@ -1,26 +1,35 @@
+#define ll long long
 class Solution {
 public:
-    int n,m;
-    vector<int> nums1,nums2;
-    vector<vector<vector<long long>>> dp;
-    long long func(int i,int j,int k){
-        if(k==0){
+    ll dp[101][101][101];
+    ll solve(int i,int j,vector<int>& nums1, vector<int>& nums2,int k){
+        if((i>=nums1.size() || j>=nums2.size()) && k==0){
             return 0;
         }
-        if(i>=n||j>=m){
+        if(i>=nums1.size() || j>=nums2.size()){
+            return -1e18;
+        }
+        if(k<0){
             return -1e18;
         }
         if(dp[i][j][k]!=LLONG_MIN){
             return dp[i][j][k];
         }
-        return dp[i][j][k] = max(func(i+1,j+1,k-1)+(long long)(nums1[i]*1LL*nums2[j]),max(func(i+1,j,k),func(i,j+1,k)));
+        ll ans=-1e18;
+        ans=max(ans,(1LL*nums1[i]*nums2[j])+solve(i+1,j+1,nums1,nums2,k-1));
+        ans=max(ans,solve(i+1,j,nums1,nums2,k));
+        ans=max(ans,solve(i,j+1,nums1,nums2,k));
+        return dp[i][j][k]=ans;
     }
     long long maxScore(vector<int>& nums1, vector<int>& nums2, int k) {
-        this->nums1 = nums1;
-        this->nums2 = nums2;
-        this->n = nums1.size();
-        this->m = nums2.size();
-        dp.assign(n,vector<vector<long long>>(m,vector<long long>(k+1,LLONG_MIN)));
-        return func(0,0,k);
+        for(int i=0;i<101;i++){
+            for(int j=0;j<101;j++){
+                for(int k=0;k<101;k++){
+                    dp[i][j][k]=LLONG_MIN;
+                }
+            }
+        }
+        return solve(0,0,nums1,nums2,k);    
+        
     }
 };
