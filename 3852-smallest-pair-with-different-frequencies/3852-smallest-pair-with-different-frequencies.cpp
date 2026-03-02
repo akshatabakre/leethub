ad0a7d1b68cb05b058ba883cf96acf4b1f494e50
Akshata Bakre <148846716+akshatabakre@users.noreply.github.com>
2026-03-02 14:41:21 +0530

    Time: 4 ms (70%), Space: 31 MB (30%) - LeetHub

--- 3852-smallest-pair-with-different-frequencies/3852-smallest-pair-with-different-frequencies.cpp
+++ 3852-smallest-pair-with-different-frequencies/3852-smallest-pair-with-different-frequencies.cpp
@@ -2,29 +2,20 @@ class Solution {
 public:
     vector<int> minDistinctFreqPair(vector<int>& nums) {
         map<int,int> f;
-        map<int,int> ftomini;
-        for(int i:nums) f[i]++;
-        for(auto it:f){
-            if(ftomini.find(it.second)==ftomini.end()){
-                ftomini[it.second] = 1e9;
-            }
-            ftomini[it.second] = min(it.first,ftomini[it.second]);
+        int mini = 1e9;
+        for(int i:nums){
+            mini = min(i,mini);
+            f[i]++;
         }
-        for(auto it:ftomini){
-            cout<<it.first<<" "<<it.second<<endl;
-        }
-        vector<int> ans(2,1e9);
-        for(auto it:ftomini){
-            if(it.second<ans[0]){
-                ans[1] = ans[0];
-                ans[0] = it.second;
-            }else if(it.second<ans[1]){
-                ans[1] = it.second;
+        int ans = 1e9;
+        for(int i:nums){
+            if(f[i]!=f[mini]){
+                ans = min(i,ans);
             }
         }
-        if(ans[0]==1e9||ans[1]==1e9){
+        if(ans==1e9){
             return {-1,-1};
         }
-        return ans;
+        return {mini,ans};
     }
 };
