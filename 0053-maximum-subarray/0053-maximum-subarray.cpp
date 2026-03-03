e4ac69109c1a457f5f537d88760c8cc076af5128
Akshata Bakre <148846716+akshatabakre@users.noreply.github.com>
2026-03-03 14:41:08 +0530

    Time: 0 ms (100%), Space: 71.9 MB (15.21%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -5,11 +5,21 @@ public:
         int mxsm = -1e9;
         int n = nums.size();
         int sm = 0;
+        int s = -1, e = -1;
+        int start = 0;
         for(int i=0;i<n;i++){
+            if(sm==0)   start = i;
             sm+=nums[i];
-            mxsm = max(mxsm,sm);
-            sm = max(0,sm);
+            if(mxsm<sm){
+                mxsm = sm;
+                s = start;
+                e = i;
+            }
+            if(sm<0){
+                sm = 0;
+            }
         }
+        cout<<s<<" "<<e<<endl;
         return mxsm;
     }
 };
