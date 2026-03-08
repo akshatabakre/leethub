9537d92d98154c83e4771a182d10979210815e70
Akshata Bakre <148846716+akshatabakre@users.noreply.github.com>
2026-03-08 14:47:18 +0530

    Time: 131 ms (12.11%), Space: 55.4 MB (5.82%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,64 +1,33 @@
 class Solution {
 public:
-    bool func(int i,int j,string& s,string& p,vector<vector<int>>& dp){
-        if(j<0 && i<0){
-            return true;
+    string s,p;
+    int n,m;
+    vector<vector<int>> dp;
+    bool match(int i,int j){
+        if(j>=m){
+            return (i>=n);
         }
-        if(j<0 && i>=0){
-            return false;
-        }
-        if(i<0){
-            for(int k=0;k<=j;k++){
-                if(p[k]!='*'){
-                    return false;
-                }
+        if(i>=n){
+            while(j<m && p[j]=='*'){
+                j++;
             }
-            return true;
+            return (j>=m);
         }
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
+        if(dp[i][j]!=-1)    return dp[i][j];
         if(s[i]==p[j] || p[j]=='?'){
-            return dp[i][j] = func(i-1,j-1,s,p,dp);
+            return dp[i][j] = match(i+1,j+1);
         }
         if(p[j]=='*'){
-            return dp[i][j] = func(i,j-1,s,p,dp)||func(i-1,j,s,p,dp);
+            return dp[i][j] = (match(i+1,j)||match(i,j+1));
         }
         return dp[i][j] = false;
     }
     bool isMatch(string s, string p) {
-        int n=s.length(),m=p.length();
-        // vector<vector<int>> dp(n,vector<int>(m,-1));
-        // return func(n-1,m-1,s,p,dp);
-        // vector<vector<bool>> dp(n+1,vector<bool>(m+1));
-        vector<bool> v(m+1),curr(m+1);
-        v[0] = true;
-        // for(int i=1;i<=n;i++){
-        //     dp[i][0] = false;
-        // }
-        for(int j=1;j<=m;j++){
-            bool allstars = true;
-            for(int k=0;k<j;k++){
-                if(p[k]!='*'){
-                    allstars = false;
-                    break;
-                }
-            }
-            v[j] = allstars;
-        }
-        for(int i=1;i<=n;i++){
-            curr[0] = false;
-            for(int j=1;j<=m;j++){
-                if(s[i-1]==p[j-1] || p[j-1]=='?'){
-                    curr[j] = v[j-1];
-                }else if(p[j-1]=='*'){
-                    curr[j] = v[j]||curr[j-1];
-                }else{
-                    curr[j] = false;
-                }
-            }
-            v=curr;
-        }
-        return v[m];
+        this->s = s;
+        this->p = p;
+        this->n = s.length();
+        this->m = p.length();
+        dp.resize(n,vector<int>(m,-1));
+        return match(0,0);
     }
 };
