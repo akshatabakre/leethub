7b0c5399314614c9f9657e334bd74b1a578f96d5
Akshata Bakre <148846716+akshatabakre@users.noreply.github.com>
2026-03-08 18:21:51 +0530

    Time: 198 ms (45.02%), Space: 13.9 MB (29.29%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -1,36 +1,25 @@
 class Solution {
 public:
-    // int f(int i,int j,vector<int>& nums,vector<vector<int>>& dp){
-    //     if(i>j){
-    //         return 0;
-    //     }
-    //     if(dp[i][j]!=-1){
-    //         return dp[i][j];
-    //     }
-    //     int maxc = 0;
-    //     for(int ind=i;ind<=j;ind++){
-    //         int coins = nums[i-1]*nums[ind]*nums[j+1] + f(i,ind-1,nums,dp) + f(ind+1,j,nums,dp);
-    //         maxc = max(coins,maxc);
-    //     }
-    //     return dp[i][j] = maxc;
-    // }
-    int maxCoins(vector<int>& nums) {
-        int n = nums.size();
-        nums.push_back(1);
-        nums.insert(nums.begin(),1);
-        vector<vector<int>> dp(n+2,vector<int>(n+2,0));
-        // return f(1,n,nums,dp);
-        for(int i=n;i>=1;i--){
-            for(int j=1;j<=n;j++){
-                if(i>j) continue;
-                int maxi = 0;
-                for(int ind=i;ind<=j;ind++){
-                    int coins = dp[i][ind-1]+dp[ind+1][j] + nums[i-1]*nums[ind]*nums[j+1];
-                    maxi = max(coins,maxi);
-                }
-                dp[i][j] = maxi;
-            }
+    vector<int>nums;
+    vector<vector<int>> dp;
+    int coins(int l,int r){
+        if(l+1==r){
+            return 0;
         }
-        return dp[1][n];
+        if(dp[l][r]!=-1){
+            return dp[l][r];
+        }
+        int maxi = 0;
+        for(int i=l+1;i<r;i++){
+            maxi = max(nums[l]*nums[r]*nums[i]+coins(l,i)+coins(i,r),maxi);
+        }
+        return dp[l][r] = maxi;
+    }
+    int maxCoins(vector<int>& nums) {
+        nums.insert(nums.begin(),1);
+        nums.push_back(1);
+        this->nums = nums;
+        dp.resize(nums.size(),vector<int>(nums.size(),-1));
+        return coins(0,nums.size()-1);
     }
 };
