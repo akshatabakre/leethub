f58d3fa670407bd504733f8da497815f0519094b
Akshata Bakre <148846716+akshatabakre@users.noreply.github.com>
2026-03-08 15:10:06 +0530

    Time: 59 ms (8.26%), Space: 32.4 MB (6.16%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,32 +1,26 @@
 class Solution {
 public:
-    // int solve(int ind, int tr,int& maxtr,vector<int>& pr,vector<vector<int>>& dp){
-    //     if(ind==pr.size()||tr==maxtr){
-    //         return 0;
-    //     }
-    //     if(dp[ind][tr]!=-1){
-    //         return dp[ind][tr];
-    //     }
-    //     if(tr%2==0){
-    //         return dp[ind][tr] = max(-pr[ind]+solve(ind+1,tr+1,maxtr,pr,dp),solve(ind+1,tr,maxtr,pr,dp));
-    //     }
-    //     return dp[ind][tr] = max(pr[ind]+solve(ind+1,tr+1,maxtr,pr,dp),solve(ind+1,tr,maxtr,pr,dp));
-    // }
-    int maxProfit(int k, vector<int>& prices) {
-        int maxtr = 2*k, n = prices.size();;
-        // vector<vector<int>> dp(n+1,vector<int>(maxtr+1,0));
-        // return solve(0,0,maxtr,prices,dp);
-        vector<int> v(maxtr+1,0),cur(maxtr+1,0);
-        for(int i=n-1;i>=0;i--){
-            for(int t=maxtr-1;t>=0;t--){
-                if(t%2==0){
-                    cur[t] = max(-prices[i]+v[t+1],v[t]);
-                }else{
-                    cur[t] = max(prices[i]+v[t+1],v[t]);
-                }
-            }
-            v=cur;
+    vector<int> prices;
+    vector<vector<vector<int>>> dp;
+    int profit(int ind,int k,int bought){
+        if(k<=0){
+            return 0;
         }
-        return v[0];
+        if(ind>=prices.size()){
+            if(bought){
+                return -1e9;
+            }
+            return 0;
+        }
+        if(dp[ind][k][bought]!=-1)  return dp[ind][k][bought];
+        if(bought){
+            return dp[ind][k][bought] = max(prices[ind]+profit(ind+1,k-1,false),profit(ind+1,k,bought));
+        }
+        return dp[ind][k][bought] = max(-prices[ind]+profit(ind+1,k,true),profit(ind+1,k,bought));
+    }
+    int maxProfit(int k, vector<int>& prices) {
+        this->prices = prices;
+        dp.resize(prices.size(),vector<vector<int>>(k+1,vector<int>(2,-1)));
+        return profit(0,k,false);
     }
 };
