39381194971f3a0c0396ade825df821838a7d08f
Akshata Bakre <148846716+akshatabakre@users.noreply.github.com>
2026-04-05 23:34:22 +0530

    Time: 1 ms (18.8%), Space: 9.1 MB (76.09%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,23 +1,28 @@
 class Solution {
 public:
-    void solve(int ind,string digits,unordered_map<char,string>&map,string w,vector<string>&ans){
-        if(w.length()==digits.length()){
-            if(w.length()>0)
-                ans.push_back(w);
+
+    string str = "";
+    vector<string> ans;
+    string digits;
+    vector<string> choices;
+
+    void rec(int ind){
+        if(ind==digits.size()){
+            ans.push_back(str);
             return;
         }
-        string s = map[digits[ind]];
-        for(int i=0;i<s.length();i++){
-            w+=s[i];
-            solve(ind+1,digits,map,w,ans);
-            w.pop_back();
+
+        for(char ch:choices[digits[ind]-'0']){
+            str+=ch;
+            rec(ind+1);
+            str.pop_back();
         }
+        return;
     }
     vector<string> letterCombinations(string digits) {
-        vector<string> ans;
-        unordered_map<char,string> m;
-        m['2'] = "abc", m['3'] = "def", m['4'] = "ghi", m['5'] = "jkl", m['6'] = "mno", m['7'] = "pqrs", m['8'] = "tuv", m['9'] = "wxyz";
-        solve(0,digits,m,"",ans);
+        this->digits = digits;
+        choices = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+        rec(0);
         return ans;
     }
 };
