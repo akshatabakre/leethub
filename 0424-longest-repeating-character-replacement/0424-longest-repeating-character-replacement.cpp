a175b76f047a25bfd03d773fd372c70523bf71ba
Akshata Bakre <148846716+akshatabakre@users.noreply.github.com>
2026-06-27 17:21:47 +0530

    Time: 53 ms (5.22%), Space: 10.9 MB (70.65%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,36 +1,34 @@
 class Solution {
 public:
-
-    bool check(map<char,int>&mp,char ch,int k,int l,int r){
-        mp[ch]++;
-        int f = 0;
-        for(auto it:mp){
-            f = max(it.second,f);
-        }
-        int flips = r-l+1 - f;
-        mp[ch]--;
-        return (flips<=k);
-    }
-    int characterReplacement(string s, int k) {
-        int n = s.length();
-        int l=0;
-        int r=-1;
-        int ans = 0;
-        map<char,int> mp;
-        while(l<n){
-            while(r+1<n && check(mp,s[r+1],k,l,r+1)){
-                r++;
-                mp[s[r]]++;
+    int characterReplacement(string s, int x) {
+        int n=s.length();
+        int ans=0;
+        for(char ch='A' ; ch<='Z' ;ch++){
+            int maxi=0;
+            int l=0;
+            int r=-1;
+            int k=x;
+            while(l<n){
+                while(r+1<n && (s[r+1]==ch || k>0)){
+                    r++;
+                    if(s[r]!=ch){
+                        k--;
+                    }
+                }
+                maxi=max(maxi,r-l+1);
+                if(r<l){
+                    l++;
+                    r=l-1;
+                }
+                else{
+                    if(s[l]!=ch)k++;
+                    l++;
+                }
             }
-            ans = max(r-l+1,ans);
-            if(l>r){
-                l++;
-                r=l-1;
-            }else{
-                mp[s[l]]--;
-                l++;
-            }
+         
+            ans=max(ans,maxi);
         }
         return ans;
+        
     }
 };
